8299e09593d04e4fc6a8c2eecada1fce97207815
Fathima sherin <sherinazeez5700@gmail.com>
2022-05-07 15:19:27 +0530

    second

---
+++ daily practice/src/Practice.java
@@ -0,0 +1,27 @@
+
+public class Practice {
+	public static void main(String[] args) {
+		
+		//implicit typecasting
+		char c='A';
+		int a=c;
+		float b=c;
+		long d=c;
+		double e=c;
+		
+		System.out.println(c);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println("----------");
+		
+		//explicit typecasting
+		double m=45.5;
+		int n=(int)m;
+		System.out.println(m);
+		System.out.println(n);
+		
+	}
+
+}
